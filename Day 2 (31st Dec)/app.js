60df80aa8a37252075108434cf0e2db1996be158
Snigdha Khatri <121375982+SnigdhaKhatri@users.noreply.github.com>
2023-01-02 13:19:23 +0530

    Added Session 2 code changes

---
+++ Day 2 (31st Dec)/app.js
@@ -0,0 +1,12 @@
+import React from "react";
+import * as ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const head1 = React.createElement("h1", { id: "head1" }, "Heading 1");
+const head2 = React.createElement("h2", { id: "head2" }, "Heading 2");
+const container = React.createElement("div", { id: "container" }, [
+  head1,
+  head2,
+]);
+root.render(container);
